35ae8706fc913c27693ff160ae11a3d9a3ffbe70
Jason Rich Darmawan <63768126+jasonrichdarmawan@users.noreply.github.com>
2023-06-29 01:31:03 +0700

    Initial Commit

---
+++ Wallpaper/WallpaperApp.swift
@@ -0,0 +1,17 @@
+//
+//  WallpaperApp.swift
+//  Wallpaper
+//
+//  Created by Jason Rich Darmawan Onggo Putra on 29/06/23.
+//
+
+import SwiftUI
+
+@main
+struct WallpaperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
